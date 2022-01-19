8749390b1e236557b25ac4d06c44669bcf1686cd
mtmtmshr <s17t274@stu.kagawa-u.ac.jp>
2022-01-19 16:56:25 +0900

    initisal commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TakamatsuMap from './TakamatsuMap';
+
+function App() {
+  return (
+    <div className="App">
+      <TakamatsuMap magnification={2}/>
+    </div>
+  );
+}
+
+export default App;
